a21d105213898c9a9b3c256d143bc275b8ba859b
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-15 21:26:06 -0400

    generateCoordinates() is implemented

--- PiApproximation/Program.cs
+++ PiApproximation/Program.cs
@@ -10,7 +10,23 @@ namespace PiApproximation
     {
         static void Main(string[] args)
         {
+            double[,] array = generateCoordinates();
+        }
 
+        private static double[,] generateCoordinates()
+        {
+            Console.WriteLine("\nPlease enter an integer between 10 and 1,000,000");
+            int len = int.Parse(Console.ReadLine().Trim());
+            Random rnd = new Random();
+            double[,] array = new double[len, 2];
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    array[i, j] = rnd.NextDouble();
+                }
+            }
+            return array;
         }
 
         public static double getHypotenus(Coordinates coordinates)
