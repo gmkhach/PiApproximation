331ec818891cfcfd27eb1db0ee685bb01e96f957
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-15 22:20:54 -0400

    methods are moved to the Utilities class and a preset simulation method is implemented.

--- PiApproximation/Program.cs
+++ PiApproximation/Program.cs
@@ -10,62 +10,17 @@ namespace PiApproximation
     {
         static void Main(string[] args)
         {
-            Coordinates[] array = generateCoordinates();
-            int counter = getCounter(array);
-            double ratio = getRatio(counter, array);
-            printResults(counter, ratio);
+            int len = Utilities.getLength();
+            runSimulation(len);
+            Utilities.runPresets();
         }
 
-        // Step 6.
-        private static void printResults(int counter, double ratio)
+        public static void runSimulation(int len)
         {
-            Console.WriteLine($"Number of overlaps:\t\t\t{counter}");
-            Console.WriteLine($"Pi Approximation:\t\t\t{ratio}");
-            Console.WriteLine($"Difference from the actual value of Pi:\t{Math.Abs(Math.PI -ratio)}");
-        }
-
-        // Step 5. 
-        private static double getRatio(int counter, Coordinates[] array)
-        {
-            int len = array.Length;
-            return 4 * (double)counter/len;
-        }
-
-        // Steps 4.
-        private static int getCounter(Coordinates[] array)
-        {
-            int counter = 0;
-            int len = array.Length;
-            for (int i = 0; i < len; i++)
-            {
-                if (getHypotenus(array[i]) <= 1)
-                {
-                    counter++;
-                }
-            }
-            return counter;
-        }
-
-        // Step 3. 
-        private static Coordinates[] generateCoordinates()
-        {
-            Console.WriteLine("\nPlease enter an integer between 10 and 1,000,000");
-            int len = int.Parse(Console.ReadLine().Trim());
-            Random rnd = new Random();
-            Coordinates[] array = new Coordinates[len];
-            for (int i = 0; i < len; i++)
-            {
-                double x = rnd.NextDouble();
-                    double y = rnd.NextDouble();
-                    array[i] = new Coordinates(x, y);
-            }
-            return array;
-        }
-
-        // Step 2.
-        public static double getHypotenus(Coordinates coordinates)
-        {
-            return Math.Sqrt(Math.Pow(coordinates.getX(), 2) + Math.Pow(coordinates.getY(), 2));
+            Coordinates[] array = Utilities.generateCoordinates(len);
+            int counter = Utilities.getCounter(array);
+            double ratio = Utilities.getRatio(counter, len);
+            Utilities.printResults(counter, ratio);
         }
     }
 }
---
+++ PiApproximation/Utilities.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiApproximation
+{
+    class Utilities
+    {
+        // Step 2.
+        public static double getHypotenus(Coordinates coordinates)
+        {
+            return Math.Sqrt(Math.Pow(coordinates.getX(), 2) + Math.Pow(coordinates.getY(), 2));
+        }
+
+        // Step 3. 
+        public static int getLength()
+        {
+            Console.Write("\nPlease enter an integer between 10 and 1,000,000 to indicates the number of points in your sample\n\n>>> ");
+            return int.Parse(Console.ReadLine().Trim());
+        }
+
+        // Step 3. continued
+        public static Coordinates[] generateCoordinates(int len)
+        {
+            Random rnd = new Random();
+            Coordinates[] array = new Coordinates[len];
+            for (int i = 0; i < len; i++)
+            {
+                double x = rnd.NextDouble();
+                double y = rnd.NextDouble();
+                array[i] = new Coordinates(x, y);
+            }
+            return array;
+        }
+
+        // Steps 4.
+        public static int getCounter(Coordinates[] array)
+        {
+            int counter = 0;
+            int len = array.Length;
+            for (int i = 0; i < len; i++)
+            {
+                if (getHypotenus(array[i]) <= 1)
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        // Step 5. 
+        public static double getRatio(int counter, int len)
+        {
+            return 4 * (double)counter / len;
+        }
+        
+        // Step 6.
+        public static void printResults(int counter, double ratio)
+        {
+            Console.WriteLine($"\nNumber of overlaps:\t\t\t{counter}");
+            Console.WriteLine($"Pi Approximation:\t\t\t{ratio}");
+            Console.WriteLine($"Difference from the actual value of Pi:\t{Math.Abs(Math.PI - ratio)}");
+        }
+
+        // Step 7.
+        public static void runPresets()
+        {
+            int[] runs = { 10, 100, 1000, 10000 };
+            Console.WriteLine("\nHere are the Pi approximation results for running the simulation for 10, 100, 1000, and 10000 points.");
+            foreach (int number in runs)
+            {
+                Console.WriteLine($"\n{number} points:");
+                Program.runSimulation(number);
+            }
+        }
+    }
+}
